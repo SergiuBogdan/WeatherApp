51696a6cd4460dad24710e39e320685c39cc4856
Sergiu <sergiu.bogdan09@yahoo.com>
2023-10-06 21:19:43 +0300

    Added React Query to fetch data.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import axios from "axios";
@@ -8,41 +8,38 @@ import humidity from "./assets/humidity.png";
 import pressure from "./assets/images.jpg";
 import outside from "./assets/outside.png";
 import wind from "./assets/wind.png";
+import { useQuery } from "@tanstack/react-query";
 
 function App() {
   const apiKey = "06e29a1acc01eb85f474701e29a23cd0";
-  const [data, setData] = useState({});
   const [inputCity, setInputCity] = useState("Cuci");
 
-  const getWeatherData = (cityname) => {
-    if (!cityname) return;
-    const apiURL =
-      "https://api.openweathermap.org/data/2.5/weather?q=" +
-      cityname +
-      "&appid=" +
-      apiKey;
+  const {
+    isError,
+    data: weatherData,
+    refetch,
+  } = useQuery({
+    queryKey: ["weather", inputCity],
+    queryFn: async () => {
+      const apiURL =
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+        inputCity +
+        "&appid=" +
+        apiKey;
 
-    axios
-      .get(apiURL)
-      .then((res) => {
-        setData(res.data);
-      })
-      .catch((err) => {
-        console.log("err", err);
-      });
-  };
+      const response = await axios.get(apiURL);
+      return response.data;
+    },
+    enabled: false, // Query will not run on component mount
+  });
 
   const handleChangeInput = (e) => {
     setInputCity(e.target.value);
   };
 
   const handleSearch = () => {
-    getWeatherData(inputCity);
+    refetch(); // Call the query manually when the search button is clicked
   };
-
-  useEffect(() => {
-    getWeatherData();
-  }, []);
 
   return (
     <div className="col-md-12">
@@ -58,42 +55,42 @@ function App() {
         />
         <Button onMouseClick={handleSearch} />
 
-        {Object.keys(data).length > 0 && (
-          <div>
-            <div className="weatherResultBox">
-              <img
-                className="weatherIcon"
-                src="https://icons.iconarchive.com/icons/alecive/flatwoken/512/Apps-Weather-icon.png"
-                alt="weatherappicon"
-              />
-              <h1 className="weatherCity">{data?.name.toUpperCase()}</h1>
+        {isError && <div>Error loading data</div>}
 
-              <h2 className="weatherTemp">
-                <img src={temp} alt="temp" className="img-temp" />
-                :&nbsp;
-                {(data?.main?.temp - 273.15).toFixed(2)}°C
-              </h2>
-              <h3 className="weatherClouds">
-                <img src={outside} alt="outside" className="img-temp" />
-                :&nbsp;
-                {data?.weather[0].main}
-              </h3>
-              <h3 className="weatherHumidity">
-                <img src={humidity} alt="humidity" className="img-temp" />
-                :&nbsp;
-                {data?.main.humidity} RH
-              </h3>
-              <h5 className="weatherPressure">
-                <img src={pressure} alt="pressure" className="img-temp" />
-                :&nbsp;
-                {data?.main.pressure} (mm Hg)
-              </h5>
-              <h6 className="weatherWindSpeed">
-                <img src={wind} alt="wind" className="img-temp" />
-                :&nbsp;
-                {data?.wind.speed} (km/h)
-              </h6>
-            </div>
+        {weatherData && Object.keys(weatherData).length > 0 && (
+          <div className="weatherResultBox">
+            <img
+              className="weatherIcon"
+              src="https://icons.iconarchive.com/icons/alecive/flatwoken/512/Apps-Weather-icon.png"
+              alt="weatherappicon"
+            />
+            <h1 className="weatherCity">{weatherData?.name.toUpperCase()}</h1>
+
+            <h2 className="weatherTemp">
+              <img src={temp} alt="temp" className="img-temp" />
+              :&nbsp;
+              {(weatherData?.main?.temp - 273.15).toFixed(2)}°C
+            </h2>
+            <h3 className="weatherClouds">
+              <img src={outside} alt="outside" className="img-temp" />
+              :&nbsp;
+              {weatherData?.weather[0].main}
+            </h3>
+            <h3 className="weatherHumidity">
+              <img src={humidity} alt="humidity" className="img-temp" />
+              :&nbsp;
+              {weatherData?.main.humidity} RH
+            </h3>
+            <h5 className="weatherPressure">
+              <img src={pressure} alt="pressure" className="img-temp" />
+              :&nbsp;
+              {weatherData?.main.pressure} (mm Hg)
+            </h5>
+            <h6 className="weatherWindSpeed">
+              <img src={wind} alt="wind" className="img-temp" />
+              :&nbsp;
+              {weatherData?.wind.speed} (km/h)
+            </h6>
           </div>
         )}
       </div>
